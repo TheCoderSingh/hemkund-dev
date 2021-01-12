33cf3ee05592388c0e792685881ce19ecbf65a31
Jaskaran Singh <jaskaran1523@gmail.com>
2021-01-12 11:02:00 -0800

    Create errors and hooks

--- screens/Signup/Signup.js
+++ screens/Signup/Signup.js
@@ -1,7 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import {
 	Dimensions,
-	Image,
 	ImageBackground,
 	KeyboardAvoidingView,
 	StyleSheet,
@@ -15,6 +14,16 @@ import Header from "../../components/Header/Header";
 const window = Dimensions.get("window");
 
 const Signup = () => {
+	const [errorOccurred, setErrorOccurred] = useState(false);
+	const [usernameInvalid, setUsernameInvalid] = useState(false);
+	const [usernameInUse, setUsernameInUse] = useState(false);
+	const [firstNameInvalid, setFirstNameInvalid] = useState(false);
+	const [lastNameInvalid, setLastNameInvalid] = useState(false);
+	const [emailInvalid, setEmailInvalid] = useState(false);
+	const [emailInUse, setEmailInUse] = useState(false);
+	const [passwordInvalid, setPasswordInvalid] = useState(false);
+	const [passwordsUnmatch, setPasswordsUnmatch] = useState(false);
+
 	return (
 		<ImageBackground source={backgroundImage} style={styles.image}>
 			<Header />
@@ -22,6 +31,59 @@ const Signup = () => {
 				style={styles.containerInner}
 				behavior="padding"
 			>
+				{errorOccurred ? (
+					<View style={styles.errors}>
+						{usernameInvalid ? (
+							<Text style={styles.errorText}>
+								Username is invalid!
+							</Text>
+						) : null}
+						{usernameInUse ? (
+							<Text style={styles.errorText}>
+								Username already in use!
+							</Text>
+						) : null}
+						{firstNameInvalid ? (
+							<Text style={styles.errorText}>
+								First Name is invalid!
+							</Text>
+						) : null}
+						{lastNameInvalid ? (
+							<Text style={styles.errorText}>
+								Last Name is invalid!
+							</Text>
+						) : null}
+						{emailInvalid ? (
+							<Text style={styles.errorText}>
+								Email is invalid!
+							</Text>
+						) : null}
+						{emailInUse ? (
+							<Text style={styles.errorText}>
+								Email already in use!
+							</Text>
+						) : null}
+						{passwordInvalid ? (
+							<Text
+								style={[
+									styles.errorText,
+									{
+										fontSize: 16,
+									},
+								]}
+							>
+								Password must contain at least 8 characters, a
+								number and both lower and uppercase letters and
+								special characters!
+							</Text>
+						) : null}
+						{passwordsUnmatch ? (
+							<Text style={styles.errorText}>
+								Passwords do not match!
+							</Text>
+						) : null}
+					</View>
+				) : null}
 				<TextInput
 					placeholder="Choose a Username"
 					textContentType="username"
@@ -109,5 +171,17 @@ const styles = StyleSheet.create({
 	},
 	extra: {
 		height: 40,
+	},
+	errors: {
+		backgroundColor: "#333",
+		borderRadius: 6,
+		justifyContent: "center",
+		width: window.width - 60,
+		minHeight: 40,
+	},
+	errorText: {
+		textAlign: "center",
+		fontSize: 18,
+		color: "#fff",
 	},
 });
