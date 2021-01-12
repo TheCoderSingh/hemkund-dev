dfa8002f7af02001886a3c4e9d041d7af9452960
Jaskaran Singh <jaskaran1523@gmail.com>
2021-01-12 02:30:36 -0800

    Install React Native Router

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { NativeRouter } from "react-router-native";
 import Login from "./screens/Login/Login";
 
 export default function App() {
 	return (
-		<View style={styles.container}>
-			<Login />
-			<StatusBar style="auto" />
-		</View>
+		<NativeRouter>
+			<View style={styles.container}>
+				<Route exact path="/" component={Login} />
+				<StatusBar style="auto" />
+			</View>
+		</NativeRouter>
 	);
 }
 
